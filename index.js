6cc6755c64045c0336519dfcc5ca6b88cc45ce93
nstlgy <yvsh7.btw@gmail.com>
2024-10-28 14:27:27 +0530

    refactor: use path module instead of hardcoded file path in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readFile("./files/hello.txt", "utf-8", (err, data) => {
+fs.readFile(path.join(__dirname, "files/hello.txt"), "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
