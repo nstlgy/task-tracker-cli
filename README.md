2c6e92b986be117f1939209165c53c254de0050f
nstlgy <yvsh7.btw@gmail.com>
2024-10-28 22:03:56 +0530

    well it's not working but here's a commit

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-A command line application for efficiently managing and organizing your tasks and to-do lists.
-
-Project Link : https://roadmap.sh/projects/task-tracker
+A command line application for managing and organizing your tasks and to-do lists written in Javascript.
 
 ## Features
 
@@ -14,3 +12,22 @@ Project Link : https://roadmap.sh/projects/task-tracker
 - View tasks filtered by status, priority, and due dates
 - Export task data to common formats
 - Simple and intuitive command line interface
+
+## Installation
+
+```javascript
+git clone https://github.com/nstlgy/task-tracker-cli.git
+cd task-tracker-cli
+npm install
+npm link
+```
+
+## Usage
+
+### List all tasks
+
+```bash
+task-cli list
+```
+
+# Project Link : https://roadmap.sh/projects/task-tracker
--- index.js
+++ index.js
@@ -1,7 +1,69 @@
-const fs = require("fs");
+// Dependencies
+const fs = require("fs").promises;
 const path = require("path");
+const readline = require("readline");
 
-fs.readFile(path.join(__dirname, "files/hello.txt"), "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// Constants
+const filePath = path.join(__dirname, "tasks.json");
+
+// Load all the tasks
+async function loadTasks() {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    return JSON.parse(data || "[]");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, JSON.stringify([]));
+      return [];
+    } else {
+      console.error("Error loading tasks:", err);
+      throw err;
+    }
+  }
+}
+
+// list all tasks
+async function listTasks() {
+  try {
+    const tasks = await loadTasks();
+    console.log(tasks);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addTask(description) {
+  const tasks = await loadTasks();
+  tasks.push({
+    id: tasks.length + 1,
+    description,
+    completed: false,
+    inProgress: false,
+  });
+
+  await fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+  console.log("Task Added: ", description);
+}
+
+const command = process.argv[3];
+const description = process.argv[4];
+
+switch (command) {
+  case "list":
+    listTasks();
+    break;
+
+  case "add":
+    if (description) {
+      addTask(description);
+    } else {
+      console.log("please provide a task description");
+    }
+    break;
+}
+
+// Error Handling
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
 });
